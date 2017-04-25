a59eb4591d1c0632d5c6e37b2c264e41e2496c24
geek <wpreul@gmail.com>
2017-04-25 15:38:59 -0500

    Add tests

--- test/index.js
+++ test/index.js
@@ -24,3 +24,131 @@ describe('hapi integration', () => {
     });
   });
 });
+
+describe('deployments', () => {
+  it('can be created', (done) => {
+    const server = new Hapi.Server();
+    server.connection();
+    server.register(PortalApi, (err) => {
+      expect(err).to.not.exist();
+      const payload = {
+        name: 'User Services',
+        datacenter: 'us-sw-1'
+      };
+
+      server.inject({ method: 'POST', url: '/deployment', payload }, (res) => {
+        expect(res.statusCode).to.equal(201);
+        expect(res.headers.location).to.exist();
+        done();
+      });
+    });
+  });
+
+  it('can be updated', (done) => {
+    const server = new Hapi.Server();
+    server.connection();
+    server.register(PortalApi, (err) => {
+      expect(err).to.not.exist();
+      const payload = {
+        name: 'User Services',
+        datacenter: 'us-sw-1'
+      };
+
+      server.inject({ method: 'PUT', url: '/deployment/42', payload }, (res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  it('can be retrieved', (done) => {
+    const server = new Hapi.Server();
+    server.connection();
+    server.register(PortalApi, (err) => {
+      expect(err).to.not.exist();
+
+      server.inject({ method: 'GET', url: '/deployment/42' }, (res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.result.name).to.equal('User Services');
+        done();
+      });
+    });
+  });
+
+  it('can be deleted', (done) => {
+    const server = new Hapi.Server();
+    server.connection();
+    server.register(PortalApi, (err) => {
+      expect(err).to.not.exist();
+
+      server.inject({ method: 'DELETE', url: '/deployment/42' }, (res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  it('can all be retrieved', (done) => {
+    const server = new Hapi.Server();
+    server.connection();
+    server.register(PortalApi, (err) => {
+      expect(err).to.not.exist();
+
+      server.inject({ method: 'GET', url: '/deployments' }, (res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.result.length).to.equal(1);
+        done();
+      });
+    });
+  });
+});
+
+describe('datacenters', () => {
+  it('can be retrieved', (done) => {
+    const server = new Hapi.Server();
+    server.connection();
+    server.register(PortalApi, (err) => {
+      expect(err).to.not.exist();
+
+      server.inject({ method: 'GET', url: '/datacenters' }, (res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.result.length).to.equal(2);
+        done();
+      });
+    });
+  });
+});
+
+
+describe('manifests', () => {
+  it('can be created', (done) => {
+    const server = new Hapi.Server();
+    server.connection();
+    server.register(PortalApi, (err) => {
+      expect(err).to.not.exist();
+      const payload = {
+        file: {}
+      };
+
+      server.inject({ method: 'POST', url: '/deployment/42/manifest', payload }, (res) => {
+        expect(res.statusCode).to.equal(201);
+        expect(res.headers.location).to.exist();
+        done();
+      });
+    });
+  });
+
+  it('can be retrieved', (done) => {
+    const server = new Hapi.Server();
+    server.connection();
+    server.register(PortalApi, (err) => {
+      expect(err).to.not.exist();
+
+      server.inject({ method: 'GET', url: '/deployment/42/manifest/5' }, (res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.result.file).to.exist();
+        done();
+      });
+    });
+  });
+});
